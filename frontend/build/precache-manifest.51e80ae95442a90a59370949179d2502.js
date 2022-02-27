4e01a252d7c463aeefe1f470aeb8dbbc00770c35
shubhamlondhe1996 <shubham.londhe@vamstar.io>
2022-02-27 19:44:10 +0530

    added demo code

---
+++ frontend/build/precache-manifest.51e80ae95442a90a59370949179d2502.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "569e6240f7bafff61835aac9e8d353c1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a0de91fc95cd56285b63",
+    "url": "/static/css/main.dbf46efa.chunk.css"
+  },
+  {
+    "revision": "1f911c779bc16728b3a3",
+    "url": "/static/js/2.2004f6e8.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.2004f6e8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a0de91fc95cd56285b63",
+    "url": "/static/js/main.7d96cbac.chunk.js"
+  },
+  {
+    "revision": "a8139511e7e3c6f8467f",
+    "url": "/static/js/runtime-main.1c2d59f4.js"
+  }
+]);
